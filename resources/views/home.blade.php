22448afb49d3ccaf69afe35ee8f92e4bb7dc9d51
Gr1l0ff <andraz.gril@gmail.com>
2021-08-12 11:28:14 +0200

    First PAge almost done

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+  
+@extends('layouts.layout')
+@section('content')
+
+<main class="main-front">
+  <h2> Dobrodošli na naši spletni strani </h2>
+  <img src="{{ asset('img/shopcart.png') }}" alt="shoppingcart">
+</main>
+
+
+@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="main-header">
+      <h2>Shopping List</h2>
+    </header>
+    <nav class="main-nav">
+      <ul>
+        <li><a href="">Domov</a></li>
+        <li><a href="">Voziček</a></li>
+        <li><a href="">Domov</a></li>
+        <li><a href="">Domov</a></li>
+      </ul>
+    </nav>
+
+    @yield('content')
+
+
+
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>
